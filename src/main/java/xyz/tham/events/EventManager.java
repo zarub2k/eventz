7e5e82e41e5b24cfb190e0ee2fbfabdd4c208384
Tham <zarub2k@gmail.com>
2023-03-06 22:53:48 +0530

    banner is added

---
+++ src/main/java/xyz/tham/events/EventManager.java
@@ -0,0 +1,14 @@
+package xyz.tham.events;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class EventManager {
+    public void all() {
+
+    }
+
+    public void create() {
+
+    }
+}
